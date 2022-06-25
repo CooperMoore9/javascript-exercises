288672889e571d42f5cf45bdbd599b671c6baf01
CooperMoore9 <thefryman9@gmail.com>
2022-06-25 13:43:05 -0500

    Solved 01 Javascript practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    console.log(Math.sign(num));
+    let fellOffABoat = '';
+    if(Math.sign(num) === -1){
+        return fellOffABoat = 'ERROR';
+    }
+    
+    for(i = 0; i < num; i++){
+        
+        fellOffABoat += string;
+    }
+    return fellOffABoat;
 };
 
 // Do not edit below this line
