9c6c70ce4786b6a1390f3701de406691197088d2
CooperMoore9 <thefryman9@gmail.com>
2022-06-26 12:39:16 -0500

    Solved reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    let stringReversed = '';
 
+
+    for(i = stringArray.length - 1; i >= 0; i--){
+        
+        stringReversed += stringArray[i];
+
+    }
+
+
+    return stringReversed;
+
+    
 };
 
 // Do not edit below this line
