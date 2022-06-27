cb29a58b3444fbf0b486a9ae0b0b1da17fda305b
CooperMoore9 <thefryman9@gmail.com>
2022-06-27 13:46:02 -0500

    Solved 04 practice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,19 @@
 const removeFromArray = function(array, ...removedElement) {
-    for (const mainElement of array){
-        for (const subElements of removedElement )
+    let arrayLength = array.length;
+    console.log(arrayLength)
+    for(i = arrayLength; i > 0; 1--){    
+        for (const mainElement of array){
+            for (const subElements of removedElement )
 
-        if(mainElement === subElements){
-            let removedPosition = array.indexOf(mainElement);
-            array.splice(removedPosition, 1);
-            
+            if(mainElement === subElements){
+                let removedPosition = array.indexOf(mainElement);
+                array.splice(removedPosition, 1);
+                
+            }
+
         }
-
+        return array;
     }
-    console.log(array);
-    return array;
-
 };
 
 // Do not edit below this line
