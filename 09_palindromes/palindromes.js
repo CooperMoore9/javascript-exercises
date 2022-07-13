9ff0e5761fb798957706b3124f2d9011dceff267
CooperMoore9 <thefryman9@gmail.com>
2022-07-13 15:06:26 -0500

    Working on it

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const stringArray = string.split('');
+  const filteredArray = stringArray.filter(word => word.toLowerCase() !== word.toUpperCase());
+  const filteredArrayReversed = stringArray.filter(word => word.toLowerCase() !== word.toUpperCase()).reverse();
 
-};
+  if(filteredArray.join('').toLowerCase() === filteredArrayReversed.join('').toLowerCase()){
+    return true;
+  }else{
+    return false;
+  };
+  
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let previousNumber = 0;
+    let nextNumber = 1;
+    let sum = 1;
 
+    for(let i = 0; i < number - 1; i++){
+        sum = previousNumber + nextNumber;
+        previousNumber = nextNumber;
+        nextNumber = sum;
+    }
+
+    if(Math.sign(number) === -1){
+        return 'OOPS';
+    }else{
+    return sum;
+    }
+    
 };
 
 // Do not edit below this line
