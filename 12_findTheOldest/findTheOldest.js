e021948598ec5eb61344142bd355d6ad8899830a
CooperMoore9 <thefryman9@gmail.com>
2022-07-15 13:36:21 -0500

    Solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // use .map to check if it has 'date of death'
+    // then use that sort method you had to filter by oldest
+    // be good at programming
+    people.map(x => {
+        if(!x.yearOfDeath){
+            x.yearOfDeath = (new Date()).getFullYear();
+            console.log(x);
+        }
+    });
 
+    const orderByOldest = people.sort((a, b) => (a.yearOfBirth - a.yearOfDeath) > (b.yearOfBirth - b.yearOfDeath) ? 1 : -1);
+    console.table(orderByOldest);
+    return orderByOldest.at(0);
 };
 
 // Do not edit below this line
